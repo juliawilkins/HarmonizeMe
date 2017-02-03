a404215df6c4eb9098738ec2a2be60ef93c49197
Julia Wilkins <jwilk017@gold.ac.uk>
2017-02-02 22:53:49 -0600

    adding initial app - first commit

---
+++ HarmonizeMe/HarmonizeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HarmonizeMe
+//
+//  Created by Julia Wilkins on 2/2/17.
+//  Copyright © 2017 Julia Wilkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
