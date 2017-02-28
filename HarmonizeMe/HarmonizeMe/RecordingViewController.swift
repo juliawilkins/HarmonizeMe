bf5536d27874ef7204c9b8c83e4817601b19a345
Alexander Fang <alex.s.fang@gmail.com>
2017-02-27 23:58:18 -0600

    Added simple harmonization in first inversion to recorded voice and playback

--- HarmonizeMe/HarmonizeMe/RecordingViewController.swift
+++ HarmonizeMe/HarmonizeMe/RecordingViewController.swift
@@ -11,12 +11,22 @@ import AVFoundation
 
 class RecordingViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
     
+    var audioEngine: AVAudioEngine!
     var audioPlayer: AVAudioPlayer?
     var audioRecorder: AVAudioRecorder?
+    var soundFileURL: URL!
+    
+    var playerA: AVAudioPlayerNode!
+    var playerB: AVAudioPlayerNode!
+    var playerC: AVAudioPlayerNode!
+    var pitchShiftA: AVAudioUnitTimePitch!
+    var pitchShiftB: AVAudioUnitTimePitch!
+    var pitchShiftC: AVAudioUnitTimePitch!
     
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var playHarmonizedButton: UIButton!
  
  
     @IBAction func recordAudio(_ sender: UIButton) {
@@ -54,15 +64,103 @@ class RecordingViewController: UIViewController, AVAudioPlayerDelegate, AVAudioR
         } else {
             audioPlayer?.stop()
         }
+        
+        if audioEngine.isRunning == true {
+            audioEngine.detach(pitchShiftA)
+            audioEngine.detach(pitchShiftB)
+            audioEngine.detach(pitchShiftC)
+            audioEngine.detach(playerA)
+            audioEngine.detach(playerB)
+            audioEngine.detach(playerC)
+            audioEngine.stop()
+        }
     }
     
+    @IBAction func playHarmonizedAudio(_ sender: UIButton) {
+        if audioRecorder?.isRecording == false {
+            stopButton.isEnabled = true
+            recordButton.isEnabled = false
+            
+            do {
+                
+                try audioPlayer = AVAudioPlayer(contentsOf:
+                    (audioRecorder?.url)!)
+                /*
+                audioPlayer!.delegate = self
+                audioPlayer!.prepareToPlay()
+                audioPlayer!.play() */
+                
+                
+                let AVFile = try! AVAudioFile.init(forReading: soundFileURL)
+                let buffer = AVAudioPCMBuffer.init(pcmFormat: AVFile.processingFormat, frameCapacity: AVAudioFrameCount(AVFile.length))
+                do {
+                    try AVFile.read(into: buffer)
+                } catch let error as NSError {
+                    print("playHarmonizedAudio AVFile read into buffer error: \(error.localizedDescription)")
+                }
+                
+                pitchShiftA.pitch = 0.0 // in cents
+                pitchShiftB.pitch = -500.0
+                pitchShiftC.pitch = -800.0
+                
+                audioEngine.attach(playerA)
+                audioEngine.attach(playerB)
+                audioEngine.attach(playerC)
+                audioEngine.attach(pitchShiftA)
+                audioEngine.attach(pitchShiftB)
+                audioEngine.attach(pitchShiftC)
+                
+                audioEngine.connect(playerA, to: pitchShiftA, format: buffer.format)
+                audioEngine.connect(pitchShiftA, to: audioEngine.mainMixerNode, format: buffer.format)
+                audioEngine.connect(playerB, to: pitchShiftB, format: buffer.format)
+                audioEngine.connect(pitchShiftB, to: audioEngine.mainMixerNode, format: buffer.format)
+                audioEngine.connect(playerC, to: pitchShiftC, format: buffer.format)
+                audioEngine.connect(pitchShiftC, to: audioEngine.mainMixerNode, format: buffer.format)
+                
+                playerB.scheduleBuffer(buffer, completionHandler: nil)
+                playerC.scheduleBuffer(buffer, completionHandler: nil)
+                playerA.scheduleBuffer(buffer, completionHandler: {
+                    self.recordButton.isEnabled = true
+                    self.stopButton.isEnabled = true
+                    self.audioEngine.detach(self.pitchShiftA)
+                    self.audioEngine.detach(self.pitchShiftB)
+                    self.audioEngine.detach(self.pitchShiftC)
+                    self.audioEngine.detach(self.playerA)
+                    self.audioEngine.detach(self.playerB)
+                    self.audioEngine.detach(self.playerC)
+                    self.audioEngine.stop()
+                } )
+                
+                audioEngine.prepare()
+                do {
+                    try audioEngine.start()
+                    playerA.play()
+                    playerB.play()
+                    playerC.play()
+                } catch let error as NSError {
+                    print("playHarmonizedAudio audioEngine error: \(error.localizedDescription)")
+                }
+
+            } catch let error as NSError {
+                print("playHarmonizedAudio error: \(error.localizedDescription)")
+            }
+        }
+        
+    }
     
-   
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         playButton.isEnabled = false
         stopButton.isEnabled = false
+        
+        audioEngine = AVAudioEngine()
+        playerA = AVAudioPlayerNode()
+        playerB = AVAudioPlayerNode()
+        playerC = AVAudioPlayerNode()
+        pitchShiftA = AVAudioUnitTimePitch()
+        pitchShiftB = AVAudioUnitTimePitch()
+        pitchShiftC = AVAudioUnitTimePitch()
         
 //        moveToHarmonyButton.isEnabled = false
         
@@ -71,7 +169,7 @@ class RecordingViewController: UIViewController, AVAudioPlayerDelegate, AVAudioR
         let dirPaths = fileMgr.urls(for: .documentDirectory,
                                     in: .userDomainMask)
         
-        let soundFileURL = dirPaths[0].appendingPathComponent("sound.caf")
+        soundFileURL = dirPaths[0].appendingPathComponent("sound.caf")
         
         let recordSettings =
             [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
@@ -108,7 +206,7 @@ class RecordingViewController: UIViewController, AVAudioPlayerDelegate, AVAudioR
         recordButton.isEnabled = true
         stopButton.isEnabled = false
     }
-    
+
     func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
         print("Audio Play Decode Error")
     }
